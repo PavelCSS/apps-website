c7ddce2e35aac89823e7109ebfabdffdb6983944
p.yashin <p.yashin@plarium.com>
2023-09-12 19:15:42 +0300

    Update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const compression = require('compression');
 const path = require('path');
+const express = require('express');
+
 const app = express();
 
-app.use(compression());
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
