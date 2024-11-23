776000f5b94b56b02003a6d20711c4e8394106fc
p.yashin <p.yashin@plarium.com>
2024-11-23 17:27:35 +0200

    Update auth callback

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const querystring = require('node:querystring');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/auth/callback', function (req, res) {
+  const query = querystring.stringify(req.query);
+
+  res.redirect(`/#/auth/callback?${query}`);
+});
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
